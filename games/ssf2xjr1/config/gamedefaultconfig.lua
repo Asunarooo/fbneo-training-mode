3e5f8640254bbe820898fe9306ab212cb639a1d1
Asunaro <tadzio.pinedo@gmail.com>
2024-11-03 00:10:35 +0100

    Restoring gamedefaultconfig.lua

---
+++ games/ssf2xjr1/config/gamedefaultconfig.lua
@@ -0,0 +1,40 @@
+---------------------------------------------------
+-- Variables used by fbneo-training-mode.lua
+---------------------------------------------------
+gamedefaultconfig = {
+	hud = {
+		combotextx=174,
+		combotexty=49,
+		comboenabled=true,
+		p1healthx=17,
+		p1healthy=22,
+		p1healthenabled=true,
+		p2healthx=355,
+		p2healthy=22,
+		p2healthenabled=true,
+		p1meterx=82,
+		p1metery=207,
+		p1meterenabled=false,
+		p2meterx=294,
+		p2metery=207,
+		p2meterenabled=false,
+	},
+	inputs = {
+		iconsize=8,
+		framenumbersenabled=true,
+		scrollinginputxoffset={2,335},
+		scrollinginputyoffset={95,95},
+	},
+	p1 = {
+		instantrefillhealth=false,
+		refillhealthenabled=true,
+		instantrefillmeter=true,
+		refillmeterenabled=true,
+	},
+	p2 = {
+		instantrefillhealth=false,
+		refillhealthenabled=true,
+		instantrefillmeter=true,
+		refillmeterenabled=true,
+	},
+}
